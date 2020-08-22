c54ba853b3a5fbec751e85545dd9a66fa654f7af
shincubin107 <57829564+shincubin107@users.noreply.github.com>
2020-08-22 10:02:33 +0700

    c

--- admin/View/home.php
+++ admin/View/home.php
@@ -2,8 +2,18 @@
 	<div class="title-style">
 		<h3><i class="fas fa-home mr-3"></i>Tình hình hoạt động hôm nay:</h3>
 	</div>
+	
+	<div class="row ml-2" >
+		<div class="col-4" id="bigTK" >
+			<div class="title">
+				<span><img src="../images/icon/icon_Sale.png"></span>
+				<span class="text">Doanh số bán hàng</span>
+			</div>
 
-	<div style="background: red;height: 150px; width: 300px;">
-		
+		</div>
+
+
 	</div>
+
+	
 </div>
