b0694f9e17dc2f46a45db0cf5fe4c3d502ed8fe6
minhhai193 <57822679+minhhai193@users.noreply.github.com>
2020-08-25 11:44:48 +0700

    AA

--- admin/Controller/login.php
+++ admin/Controller/login.php
@@ -1,17 +1,3 @@
-<?php
-    // gọi file adminlogin
-    include '../../Model/adminlogin.php';
-?>
-<?php
-    // gọi class adminlogin
-    $class = new adminlogin(); 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        // LẤY DỮ LIỆU TỪ PHƯƠNG THỨC Ở FORM POST
-        $adminUser = $_POST['adminUser'];
-        $adminPass = $_POST['adminPass'];
-        $login_check = $class -> longin_admin($adminUser,$adminPass); // hàm check User and Pass khi submit lên
-    }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,115 +6,40 @@
 	<title>Admin Login</title>
 	<link rel="SHORTCUT ICON" href="../images/Logo_Shop.png">
 	<style>
+	body{font-family:'Montserrat',sans-serif;margin:0;background:url(../images/img_background.png) no-repeat;background-size:cover;-webkit-background-size:cover;-moz-background-size:cover;-o-background-size:cover;-ms-background-size:cover;background-attachment:fixed;background-position:center;}
+	p{padding:0;margin:0;color:#999;}
 
-		body{
-			font-family:'Montserrat',sans-serif;
-			margin:0;
-			background:url(../images/img_background.png) no-repeat;
-			background-size:cover;
-			-webkit-background-size:cover;
-			-moz-background-size:cover;
-			-o-background-size:cover;
-			-ms-background-size:cover;
-			background-attachment:fixed;
-			background-position:center;
-		}
-
-		p{
-			padding:0;
-			margin:0;
-			color:#999;
-		}
-
-		/*-- main --*/
-		.login-main{
-			width:25%;
-			background:rgba(0, 0, 0,0.8);
-			text-align:center;
-			-webkit-box-shadow: 0px 0px 15px 5px #FFFFFF; 
-			box-shadow: 0px 0px 15px 1px #FFFFFF;
-			margin: 100px auto;
-			border-radius: 5px;
-		}
-
-		.login-main h2{
-			color:#fff;
-			font-size:27px;
-			letter-spacing:2px;
-			margin-bottom:10px;
-			text-align:center;
-		}
-
-		input[type="username"],input[type="password"]{
-			width:80%;
-			padding:16px 50px 16px 50px;
-			outline:none;
-			font-size:15px;
-			font-weight:400;
-			color:white;
-			margin:14px 0px;
-			font-family:'Montserrat',sans-serif;
-			border:none;
-			border-bottom: 2px solid white;
-			background:transparent;
-			letter-spacing:1px;
-			-webkit-border-radius:5px;
-			-moz-border-radius:5px;
-			-ms-border-radius:5px;
-			-o-border-radius:5px;
-		}
-
-		input[type="username"]{
-			background:url(../images/user.png) no-repeat 10px 10px;
-		}
-		input[type="password"]{
-			background:url(../images/lock.png) no-repeat 10px 10px;
-		}
-
-		input[type="submit"]{
-			padding:10px 32%;
-			font-size:18px;
-			font-weight: 600;
-			border-radius:30px;
-			-webkit-border-radius:30px;
-			-moz-border-radius:30px;
-			-ms-border-radius:30px;
-			-o-border-radius:30px;
-			text-transform:uppercase;
-			letter-spacing:1px;
-			background:#36bf89;
-			color:#ffffff;
-			border:none;
-			outline:none;
-			cursor:pointer;
-			font-family:'Montserrat',sans-serif;
-			margin:24px auto;
-			margin-top: 10px;
-		}
-
-		input[type="submit"]:hover{
-			background:#188f60;
-			color:#fff;
-			transition:0.8s all;
-			-webkit-transition:0.8s all;
-			-o-transition:0.8s all;
-			-moz-transition:0.8s all;
-			-ms-transition:0.8s all;
-		}
-
-
+	/*-- main --*/
+	.login-main{width:25%;background:rgba(0,0,0,0.8);text-align:center;-webkit-box-shadow:0px 0px 15px 5px #FFFFFF;box-shadow:0px 0px 15px 1px #FFFFFF;margin:100px auto;border-radius:5px;}
+	.login-main h2{color:#fff;font-size:27px;letter-spacing:2px;margin-bottom:10px;text-align:center;}
+	input[type="username"],
+	input[type="password"]{width:55%;padding:16px 50px 16px 50px;outline:none;font-size:15px;font-weight:400;color:white;margin:14px 0px;font-family:'Montserrat',sans-serif;border:none;border-bottom:2px solid white;background:transparent;letter-spacing:1px;-webkit-border-radius:5px;-moz-border-radius:5px;-ms-border-radius:5px;-o-border-radius:5px;}
+	input[type="username"]{background:url(../images/user.png) no-repeat 10px 10px;}
+	input[type="password"]{background:url(../images/lock.png) no-repeat 10px 10px;}
+	input[type="submit"]{padding:10px 32%;font-size:18px;font-weight:600;border-radius:30px;-webkit-border-radius:30px;-moz-border-radius:30px;-ms-border-radius:30px;-o-border-radius:30px;text-transform:uppercase;letter-spacing:1px;background:#36bf89;color:#ffffff;border:none;outline:none;cursor:pointer;font-family:'Montserrat',sans-serif;margin:24px auto;margin-top:10px;}
+	input[type="submit"]:hover{background:#188f60;color:#fff;transition:0.8s all;-webkit-transition:0.8s all;-o-transition:0.8s all;-moz-transition:0.8s all;-ms-transition:0.8s all;}
 	</style>
-
+<?php
+	// gọi file adminlogin
+	include '../../Model/adminlogin.php';
+?>
+<?php
+	// gọi class adminlogin
+	$class = new adminlogin(); 
+	if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	// LẤY DỮ LIỆU TỪ PHƯƠNG THỨC Ở FORM POST
+	$adminUser = $_POST['adminUser'];
+	$adminPass = $_POST['adminPass'];
+	$login_check = $class -> longin_admin($adminUser,$adminPass); // hàm check User and Pass khi submit lên
+	}
+?>
 </head>
 <body>
 	<div class="main">
 		<div class="login-main">
-				
 				<img src="../images/Logo_Shop.png" style="width: 60%; height: 20%;"/>
 				<h2>Gear<a style="color: red">away</a> Admin</h2>
-
 				<form action="login.php" method="post">
-					
 					<input type="username" placeholder="Username" required="" name="adminUser" autocomplete="off">
 					<input type="password" placeholder="Password" required=""  name="adminPass"/>
 					<div style="color:red;">
@@ -139,7 +50,6 @@
 							}
 						?>
 					</div>
-
 					<input id="btn_Login" type="submit" value="LOGIN"/>
 				</form>
 		</div>
